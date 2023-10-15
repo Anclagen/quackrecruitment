379fcec2bd3200f79d2d55286856d608db13a572
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-10-15 22:42:20 +0200

    build error from meta in layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,13 +7,6 @@ import Footer from "@/components/UI/Footer";
 
 const inter = Roboto({ subsets: ["latin"], weight: ["100", "400", "500", "700", "900"] });
 
-export const metadata = {
-  title: "Quack Recruitment",
-  description: "Quack Recruitment, your local recruitment specialist.",
-  keywords: ["Quack Group", "Quack Recruitment", "Jobs", "Training", "Recruiters", "Recruitment"],
-  authors: [{ name: "Alexander Barrett", url: "https://github.com/Anclagen" }],
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
