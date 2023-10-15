f334560b2f08606093b7aaa72060859572f857f2
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-10-15 21:59:26 +0200

    added basic jobs page

--- src/components/Sections/FourImageFeature/FourImageStretch.jsx
+++ src/components/Sections/FourImageFeature/FourImageStretch.jsx
@@ -16,12 +16,12 @@ const FourImageStretch = ({ title, text, images, reverse = false, dark = false, 
           <div className="relative mt-10 lg:-mx-4 relative-20 lg:mt-0 lg:col-start-1">
             <div className="relative space-y-4">
               <div className="flex items-end justify-center space-x-4 lg:justify-start px-6 mb-6">
-                <img className="hidden lg:block rounded-lg shadow-lg shadow-gray-500 md:w-52 xl:w-64 2xl:w-72" src={images[0]} />
-                <img className=" rounded-lg shadow-lg shadow-gray-500 lg:w-60 xl:w-80 2xl:w-96" src={images[1]} alt="2" />
+                <img className="hidden lg:block rounded-lg shadow-lg shadow-gray-500 md:w-52 xl:w-64 2xl:w-72" src={images[0].src} alt={images[0].alt} />
+                <img className=" rounded-lg shadow-lg shadow-gray-500 lg:w-60 xl:w-80 2xl:w-96" src={images[1].src} alt={images[1].alt} />
               </div>
               <div className="flex items-start justify-center ml-12 space-x-4 lg:justify-start ">
-                <img className="hidden lg:block rounded-lg shadow-lg shadow-gray-500 md:w-36 xl:w-48 2xl:w-56" src={images[2]} alt="3" />
-                <img className="hidden lg:block rounded-lg shadow-lg shadow-gray-500 md:w-60 xl:w-64 2xl:w-72" src={images[3]} alt="4" />
+                <img className="hidden lg:block rounded-lg shadow-lg shadow-gray-500 md:w-36 xl:w-48 2xl:w-56" src={images[2].src} alt={images[2].alt} />
+                <img className="hidden lg:block rounded-lg shadow-lg shadow-gray-500 md:w-60 xl:w-64 2xl:w-72" src={images[3].src} alt={images[3].alt} />
               </div>
             </div>
           </div>
