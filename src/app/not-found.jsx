fd17bb9d8572f84abc87b39744b467b779011d39
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-10-15 17:35:16 +0200

    build error from unenclosed ( ' )

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -8,7 +8,7 @@ const NoPage = () => {
           <h1 className="mb-8 font-extrabold text-9xl text-gray-400">
             <span className="sr-only">Error</span>404
           </h1>
-          <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+          <p className="text-2xl font-semibold md:text-3xl">{"Sorry, we couldn't find this page."}</p>
           <p className="mt-4 mb-8 text-gray-600">But dont worry, you can find plenty of other things on our homepage.</p>
           <Link rel="noopener noreferrer" href="/" className="px-8 py-3 font-semibold rounded bg-violet-600 text-gray-50">
             Back to homepage
