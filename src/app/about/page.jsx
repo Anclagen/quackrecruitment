f353e73588bc669aa5a3622b7ccfd7aa62af844d
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-10-15 23:50:29 +0200

    updated about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,28 +1,32 @@
 import BannerHome from "../../components/Banner/BannerHome";
 import Section from "../../components/Sections/Section";
-import FadeInSection from "../../components/Sections/FadeInSection/FadeInSection";
 import CardSection from "../../components/Sections/CardSection/CardSection";
 import { ProfileCard } from "../../components/Card/Card";
 import FourImageStretch from "../../components/Sections/FourImageFeature/FourImageStretch";
-import Head from "next/head";
+import FadeInFreeTextSection from "@/components/Sections/FadeInFreeFormatSection/FadeInFreeFormateSection";
+
+export const metadata = {
+  title: "About | Quack Recruitment",
+  description: "Learn about Quack Recruitment, who we are and what we can do for you.",
+  keywords: ["Quack Group", "Quack Recruitment", "Jobs", "Training", "Recruiters", "Recruitment"],
+  authors: [{ name: "Alexander Barrett", url: "https://github.com/Anclagen" }],
+};
 
 const About = () => {
   return (
     <>
-      <Head>
-        <title>About</title>
-        <meta name="description" content="A Dedicated & Experienced Team Of Experts" key="description" />
-        {/* <meta name="keywords" content={keywords} key="keywords" /> */}
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-      </Head>
-      <BannerHome title="About" slogan="A Dedicated & Experienced Team Of Experts" imgSrc="/pexels-olga-lioncat-7245326.jpg" imgAlt="Descriptive Image Alt Text" />
+      <BannerHome title="About" slogan="A Dedicated & Experienced Team Of Experts" imgSrc="images/banners/pexels-yan-krukau-7691705.jpg" imgAlt="Group opf people looking at a computer" />
       <Section bgColor="bg-zinc-950 text-white">
         <FourImageStretch
           title="Who We Are"
           reverse={false}
           dark={true}
-          images={["/pexels-yan-krukau-8867265.jpg", "/pexels-yan-krukau-8866793.jpg", "/pexels-yan-krukau-8867482.jpg", "/pexels-mart-production-7709219.jpg"]}
+          images={[
+            { src: "images/pexels-yan-krukau-8867265.jpg", alt: "Office workers at their desks" },
+            { src: "images/pexels-yan-krukau-8866793.jpg", alt: "Team photo in the office" },
+            { src: "images/pexels-mart-production-7709219.jpg", alt: "Office workers at their desks" },
+            { src: "images/pexels-yan-krukau-8867482.jpg", alt: "another team photo in the office" },
+          ]}
         >
           <div className="px-6 lg:p-0">
             <p className="mt-4 text-lg leading-6">
@@ -47,43 +51,39 @@ const About = () => {
         </FourImageStretch>
       </Section>
       <Section bgColor="bg-zinc-200">
-        <FadeInSection
-          title="Meet The Team"
-          imageSrc="/pexels-yan-krukau-8867482.jpg"
-          imageAlt="Description"
-          text={[
-            "At Quack Specialists Ltd, the backbone of our operations rests with our Senior Team. These dedicated professionals are often the driving force, working diligently behind the scenes, ensuring the seamless functioning of our business. Their responsibilities don't just end at overseeing daily operations. They're also proactive in addressing any queries, concerns, or challenges that arise, guaranteeing that our partners, clients, and candidates receive top-notch service at all times. ",
-            "We believe in transparency and fostering trust, so we invite you to 'virtually meet' and get acquainted with some of the key members of our Senior Team below. Get to know the minds and faces that are committed to your success!",
-          ]}
-          reverse={true}
-        />
-        <h3 className="text-2xl font-semibold text-center mt-16 mb-10">Leadership Team</h3>
-        <p className="mb-32 text-xl font-normal text-center text-gray-500 max-w-screen-lg px-10 mx-auto">
+        <FadeInFreeTextSection title="Prospective Clients" imageSrc="/images/pexels-yan-krukau-8867482.jpg" imageAlt="Description" reverse={true}>
+          <p className="mt-4 text-lg leading-6">
+            Our client list makes for eclectic reading, from major high street names, well managed local operations, to recruitment agencies who are among the top 4 largest in Europe, supporting them
+            on second tiered projects on a national basis.
+          </p>
+          <p className="mt-4 text-lg leading-6">
+            Each client has a dedicated Account Manager who looks after the field their clients operate within, and allows a rapport to be developed, as well as a consistent experience!
+          </p>
+          <p className="mt-4 text-lg leading-6">We are a GLAA License holder and our insurance levels are setup to support our clients needs as follows:</p>
+          <ul className="mt-1 ms-4 list-disc">
+            <li>Public Liability = £10m</li>
+            <li>{"Employers' Liability = £10m"}</li>
+            <li>Professional Indemnity = £10m </li>
+          </ul>
+          <p className="mt-4 text-lg leading-6">To find out if we can help, get in touch with your Regional Business Development Manager who will be excited to hear about what you do!</p>
+          <a href="mailto:sales@quackrecruitment.co.uk" className="mt-1 text-violet-900 hover:underline">
+            sales@quackrecruitment.co.uk
+          </a>
+        </FadeInFreeTextSection>
+      </Section>
+      <Section bgColor="bg-zinc-950">
+        <h3 className="text-2xl text-white font-semibold text-center mt-16 mb-10">Leadership Team</h3>
+        <p className="mb-32 text-xl font-normal text-center text-gray-200 max-w-screen-lg px-10 mx-auto">
           We have selected our team not only based on industry knowledge but based on passion for success and wanting to go that extra mile to develop themselves, our clients & candidates.
         </p>
         <CardSection>
           <ProfileCard
-            userImage="/lee-inskip.jpg"
-            userName="Lee Inskip"
-            userPosition="Chief Operations Officer"
-            userDescription="Lee trained as a butcher in the '80s and worked in the hospitality trade before moving into recruitment. He has managed sites with up to 1200 workers across multiple locations, specializing in sectors like Industrial, Grounds Maintenance, Engineering, and Construction. Boasting a wealth of sector experience even before the recruitment industry gained its prominence, is deeply involved in the day-to-day operations of the business. With a background in operational roles at top global recruitment agencies, Lee effortlessly liaises with clients. His expertise ensures that our consultants are always aligned with client needs, facilitating successful placements."
-            linkedin={"https://www.linkedin.com/in/lee-inskip-4b80bb59/"}
-          />
-          <ProfileCard
-            userImage="/jamie-croall.jpg"
+            userImage="images/jamie-croall.jpg"
             userName="Jamie Croall"
             userPosition="Chief Financial Officer"
             userDescription="Jamie plays a pivotal role in the meticulous planning and management of our financial affairs, encompassing everything from ledgers and forecasts to projections. He leads a dedicated team focused on the critical aspects of our financial operations. This includes ensuring timely payments from our customers, settling accounts with our suppliers punctually, and most importantly, guaranteeing that our workers receive accurate and timely compensation."
             linkedin={"https://www.linkedin.com/in/jamie-croall-92459987/"}
           />
-          {/* <ProfileCard
-            userImage={uglyMugImage}
-            userName="Carl Underwood"
-            userPosition="Recruitment Consultant"
-            userDescription="Carl worked in the Finance and Foreign Currency industry for 18 years before transitioning to recruitment. He has been specializing in industrial positions for the past 2 years, leveraging his extensive experience in the finance sector to provide top-notch recruitment services."
-            linkedin={"#"}
-            facebook={"#"}
-          /> */}
         </CardSection>
       </Section>
     </>
