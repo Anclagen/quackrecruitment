b05bbb28ca0ebd02d2d4cb448eddce5d7bd30935
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-10-23 13:52:50 +0200

    caps error

--- src/components/ContactForms/CandidateRegistrationForm/FormSections/index.jsx
+++ src/components/ContactForms/CandidateRegistrationForm/FormSections/index.jsx
@@ -2,5 +2,5 @@ export { default as BankDetails } from "./BankDetails";
 export { default as ContactInformation } from "./ContactInformation";
 export { default as PersonalDetails } from "./PersonalDetails";
 export { default as DocumentUpload } from "./DocumentUpload";
-export { default as Checklist } from "./Checklist";
+export { default as Checklist } from "./CheckList";
 export { default as WorkAndDisabilities } from "./WorkAndDisabilities";
