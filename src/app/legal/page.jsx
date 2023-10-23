e90845ad45d2fda317a43af9581a8a23fcc15ee5
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-10-23 14:47:57 +0200

    legal and placeholder training page

--- src/app/legal/page.jsx
+++ src/app/legal/page.jsx
@@ -16,75 +16,20 @@ const Legal = () => {
   return (
     <>
       <BannerHome title="Legal" slogan="A Dedicated & Experienced Team Of Experts" imgSrc="images/banners/pexels-yan-krukau-7691705.jpg" imgAlt="Group opf people looking at a computer" />
-      <Section bgColor="bg-zinc-950 text-white">
-        <FourImageStretch
-          title="Who We Are"
-          reverse={false}
-          dark={true}
-          images={[
-            { src: "images/pexels-yan-krukau-8867265.jpg", alt: "Office workers at their desks" },
-            { src: "images/pexels-yan-krukau-8866793.jpg", alt: "Team photo in the office" },
-            { src: "images/pexels-mart-production-7709219.jpg", alt: "Office workers at their desks" },
-            { src: "images/pexels-yan-krukau-8867482.jpg", alt: "another team photo in the office" },
-          ]}
-        >
-          <div className="px-6 lg:p-0">
-            <p className="mt-4 text-lg leading-6">
-              Quack Recruitment is an independently owned Recruitment Agency operating throughout the country and supporting candidates and clients throughout Hertfordshire, Bedfordshire,
-              Buckinghamshire, Northamptonshire, Staffordshire, Derbyshire, Leicestershire, Nottinghamshire, Devon, and Lancashire.
-            </p>
-            <p className="mt-4 text-lg leading-6">
-              At Quack Recruitment, we aim to standout above the competition. We do this through hard work, providing honest feedback, delivering on our promises, and tailoring our solution having
-              analysed the requirements set out in front of us.
-            </p>
-            <p className="mt-4 text-lg leading-6">As an agency we cover a number of sectors, and we have consultants who are skilled in placing:</p>
-            <ul className="mt-1 ms-4 list-disc">
-              <li className="">Administration </li>
-              <li className="">Customer Services </li>
-              <li className="">Engineering </li>
-              <li className="">Food Processing & Packaging </li>
-              <li className="">IT</li>
-              <li className="">Logistics </li>
-              <li className="">Sales </li>
-            </ul>
-          </div>
-        </FourImageStretch>
-      </Section>
+
       <Section bgColor="bg-zinc-200">
-        <FadeInFreeTextSection title="Prospective Clients" imageSrc="/images/pexels-yan-krukau-8867482.jpg" imageAlt="Description" reverse={true}>
-          <p className="mt-4 text-lg leading-6">
-            Our client list makes for eclectic reading, from major high street names, well managed local operations, to recruitment agencies who are among the top 4 largest in Europe, supporting them
-            on second tiered projects on a national basis.
-          </p>
-          <p className="mt-4 text-lg leading-6">
-            Each client has a dedicated Account Manager who looks after the field their clients operate within, and allows a rapport to be developed, as well as a consistent experience!
-          </p>
+        <FadeInFreeTextSection title="Legal Notice" imageSrc="/images/pexels-yan-krukau-8867482.jpg" imageAlt="Description" reverse={true}>
+          <p className="mt-4 text-lg leading-6">Name of Company: Quack Recruitment Ltd</p>
+          <p className="mt-4 text-lg leading-6">Registered Office: Business & Technology Centre, Bessemer Drive, Stevenage, Herts, SG1 2DX</p>
+          <p className="mt-4 text-lg leading-6">Email: hello@quackrecruitment.co.uk</p>
+          <p className="mt-4 text-lg leading-6">Phone Number: 0333 577 0036</p>
           <p className="mt-4 text-lg leading-6">We are a GLAA License holder and our insurance levels are setup to support our clients needs as follows:</p>
           <ul className="mt-1 ms-4 list-disc">
             <li>Public Liability = £10m</li>
             <li>{"Employers' Liability = £10m"}</li>
             <li>Professional Indemnity = £10m </li>
           </ul>
-          <p className="mt-4 text-lg leading-6">To find out if we can help, get in touch with your Regional Business Development Manager who will be excited to hear about what you do!</p>
-          <a href="mailto:sales@quackrecruitment.co.uk" className="mt-1 text-violet-900 hover:underline">
-            sales@quackrecruitment.co.uk
-          </a>
         </FadeInFreeTextSection>
-      </Section>
-      <Section bgColor="bg-zinc-950">
-        <h3 className="text-2xl text-white font-semibold text-center mt-16 mb-10">Leadership Team</h3>
-        <p className="mb-32 text-xl font-normal text-center text-gray-200 max-w-screen-lg px-10 mx-auto">
-          We have selected our team not only based on industry knowledge but based on passion for success and wanting to go that extra mile to develop themselves, our clients & candidates.
-        </p>
-        <CardSection>
-          <ProfileCard
-            userImage="images/jamie-croall.jpg"
-            userName="Jamie Croall"
-            userPosition="Chief Financial Officer"
-            userDescription="Jamie plays a pivotal role in the meticulous planning and management of our financial affairs, encompassing everything from ledgers and forecasts to projections. He leads a dedicated team focused on the critical aspects of our financial operations. This includes ensuring timely payments from our customers, settling accounts with our suppliers punctually, and most importantly, guaranteeing that our workers receive accurate and timely compensation."
-            linkedin={"https://www.linkedin.com/in/jamie-croall-92459987/"}
-          />
-        </CardSection>
       </Section>
     </>
   );
--- src/app/training/page.jsx
+++ src/app/training/page.jsx
@@ -1,21 +1,9 @@
-import BannerHome from "@/components/Banner/BannerHome";
+import BannerHalf from "@/components/Banner/BannerHalf";
 
 function Training() {
   return (
     <>
-      <main className="flex flex-grow flex-col items-center justify-between">
-        <BannerHome
-          title="Training"
-          slogan="Turn Passion into Profession."
-          imgSrc="images/pexels-olga-lioncat-7245326.jpg"
-          imgAlt="Descriptive Image Alt Text"
-          links={[
-            { label: "Vacancies", path: "/vacancies", color: "violet" },
-            { label: "Register", path: "/register", color: "violet" },
-            { label: "Training", path: "/training", color: "violet" },
-          ]}
-        />
-      </main>
+      <BannerHalf title="Training" slogan="Turn Passion into Profession." imgSrc="images/banners/pexels-sevenstorm-juhaszimrus-443383.jpg" imgAlt="Descriptive Image Alt Text" />
     </>
   );
 }
