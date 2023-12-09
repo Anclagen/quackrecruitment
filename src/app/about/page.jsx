797b515522de786d587efaf23c4eb5bae7cb45ea
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-12-09 01:46:30 +0100

    content changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -22,11 +22,11 @@ const About = () => {
         imgAlt="Group opf people looking at a computer"
         position="top"
       />
-      <Section bgColor="bg-zinc-950 text-white">
+      <Section bgColor="bg-zinc-200">
         <FourImageStretch
           title="Who We Are"
           reverse={false}
-          dark={true}
+          dark={false}
           images={[
             { src: "images/pexels-yan-krukau-8867265.jpg", alt: "Office workers at their desks" },
             { src: "images/pexels-yan-krukau-8866793.jpg", alt: "Team photo in the office" },
@@ -35,6 +35,27 @@ const About = () => {
           ]}
         >
           <div className="px-6 lg:p-0">
+            <p className="mt-4 text-lg leading-6">
+              Welcome to Quack Recruitment & Training, where our mission is to redefine excellence in talent acquisition and professional development. As a national recruitment agency, we specialise
+              in catering to the diverse needs of the temporary, permanent, and managed services sectors. Our commitment to precision and understanding the unique requirements of each industry sets us
+              apart.
+            </p>
+            <h3 className="mt-4 text-lg leading-6 font-semibold"> Recruitment Expertise</h3>
+            <p className="mt-4 text-lg leading-6">
+              {" "}
+              Navigating the dynamic and ever changing landscapes of recruitment, we pride ourselves on sourcing the best candidates to meet your organisation's demands. Whether you're seeking
+              temporary staff for project peaks, permanent team members, or comprehensive managed services, we tailor our approach to ensure a seamless fit with your company culture and objectives.
+            </p>
+            <h3 className="mt-4 text-lg leading-6 font-semibold"> Training Division</h3>
+            <p className="mt-4 text-lg leading-6">
+              {" "}
+              At the core of our business, we have a dedicated Training Division committed to shaping the future workforce. Our comprehensive Level 3 courses cover key areas such as Administration &
+              Customer Service, CSCS, SIA and many more. We believe in equipping individuals with the skills and knowledge needed to excel in their roles, fostering professional growth and
+              contributing to the success of businesses.
+            </p>
+          </div>
+
+          {/* <div className="px-6 lg:p-0">
             <p className="mt-4 text-lg leading-6">
               Quack Recruitment is an independently owned Recruitment Agency operating throughout the country and supporting candidates and clients throughout Hertfordshire, Bedfordshire,
               Buckinghamshire, Northamptonshire, Staffordshire, Derbyshire, Leicestershire, Nottinghamshire, Devon, and Lancashire.
@@ -53,12 +74,29 @@ const About = () => {
               <li className="">Logistics </li>
               <li className="">Sales </li>
             </ul>
-          </div>
+          </div> */}
         </FourImageStretch>
       </Section>
-      <Section bgColor="bg-zinc-200">
-        <FadeInFreeTextSection title="Prospective Clients" imageSrc="/images/pexels-yan-krukau-8867482.jpg" imageAlt="Description" reverse={true}>
+      <Section bgColor="bg-gray-50">
+        <FadeInFreeTextSection title="Why Choose Quack Recruitment & Training?" imageSrc="/images/pexels-yan-krukau-8867482.jpg" imageAlt="Description" reverse={true}>
           <p className="mt-4 text-lg leading-6">
+            <b>Holistic Solutions:</b> We go beyond recruitment, offering a holistic approach that includes talent acquisition and comprehensive training programs.
+          </p>
+          <p className="mt-4 text-lg leading-6">
+            <b>Industry Insight:</b> Our experienced team understands the nuances of various industries, ensuring we match candidates with the right skills and cultural fit.
+          </p>
+          <p className="mt-4 text-lg leading-6">
+            <b>Professional Development:</b> The success of our Training Division lies in empowering individuals to excel in their roles, contributing to the growth of both individuals and businesses.
+          </p>
+          <p className="mt-4 text-lg leading-6">
+            <b>Tailored Services:</b> Whether you're a startup, a growing business, or an established enterprise, our services are customised to meet your specific needs.
+          </p>
+          <p className="mt-4 text-lg leading-6">
+            Partner with Quack Recruitment & Training, for a strategic alliance that goes beyond conventional recruitment. Experience a journey where every placement is not just a transaction but a
+            step towards a thriving and skilled workforce.
+          </p>
+
+          {/* <p className="mt-4 text-lg leading-6">
             Our client list makes for eclectic reading, from major high street names, well managed local operations, to recruitment agencies who are among the top 4 largest in Europe, supporting them
             on second tiered projects on a national basis.
           </p>
@@ -74,7 +112,7 @@ const About = () => {
           <p className="mt-4 text-lg leading-6">To find out if we can help, get in touch with your Regional Business Development Manager who will be excited to hear about what you do!</p>
           <a href="mailto:sales@quackrecruitment.co.uk" className="mt-1 text-quackred-900 hover:underline">
             sales@quackrecruitment.co.uk
-          </a>
+          </a> */}
         </FadeInFreeTextSection>
       </Section>
       <Section bgColor="bg-zinc-950">
