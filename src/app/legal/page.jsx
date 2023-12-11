a0ef2be0ffb41572967591766bd15dc849a84de2
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-12-11 12:10:17 +0100

    Wrong heading

--- src/app/legal/page.jsx
+++ src/app/legal/page.jsx
@@ -14,7 +14,7 @@ const Legal = () => {
   return (
     <>
       <BannerHalf
-        title="About"
+        title="Legal"
         slogan="A Dedicated & Experienced Team Of Experts"
         imgSrc="images/banners/pexels-yan-krukau-7691705.jpg"
         imgAlt="Group opf people looking at a computer"
