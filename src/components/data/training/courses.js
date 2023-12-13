c50d913dc063ca6241f7db467cdf67c36d4b3391
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-12-13 14:32:24 +0100

    added more course data and updated course page

---
+++ src/components/data/training/courses.js
@@ -0,0 +1,219 @@
+export const courses = [
+  {
+    title: "Level 3 Customer Service & Administration",
+    description: "Unlock the skills needed to excel in administrative roles. Our comprehensive administration training covers:",
+    openingMessages: [
+      "Welcome to Quack Recruitment & Training!",
+      "Unlocking Your Potential with Level 3 Customer Service & Administration Training.",
+      "At Quack Recruitment & Training, we believe in empowering all individuals to reach new heights in their careers.",
+      "Our comprehensive Level 3 Course in Customer Service & Administration is designed to equip you with the knowledge and skills necessary to excel in the dynamic and rewarding field of administration.",
+    ],
+    whyChoose: [
+      {
+        title: "Industry Experienced Instructors",
+        description:
+          "Our team of seasoned instructors comprises industry professionals with a wealth of experience in customer service and administration. They bring real-world insights and practical knowledge to the classroom, ensuring you receive top-tier education that directly applies to your future career.",
+      },
+      {
+        title: "Practical, Job-Ready Curriculum",
+        description:
+          "Our Level 3 Course is meticulously crafted to cover all aspects of customer service and administration. From foundational concepts to advanced skills, our curriculum is geared towards making you job-ready from day one. We integrate real-world scenarios and case studies to enhance your problem-solving abilities.",
+      },
+      {
+        title: "Flexible Learning Solutions",
+        description:
+          "Recognising the diverse needs of adult learners, we offer flexible learning options. Choose from evening classes, weekend sessions, or a blended learning approach that combines online and in-person instruction. Our goal is to make education accessible and convenient for you.",
+      },
+      {
+        title: "Modern Learning Facilities",
+        description:
+          "Quack Recruitment & Training takes pride in providing modern learning facilities equipped with the latest technology. Our classrooms are designed for interactive learning, and our training environments simulate real-world customer service and administrative scenarios to enhance your practical skills.",
+      },
+    ],
+    whatYouLearn: [
+      {
+        title: "Customer Service Excellence",
+        description: "Learn to provide exceptional customer service.",
+        points: ["Effective communication and active listening.", "Resolving customer complaints and handling difficult situations.", "Building and maintaining positive customer relationships."],
+      },
+      {
+        title: "Administrative Skills",
+        description: "Acquire the skills needed to succeed in administrative roles.",
+        points: ["Organisational and time management skills.", "Record-keeping and data management.", "Efficient use of office software tools."],
+      },
+      {
+        title: "Supervisory and Leadership Skills",
+        description: "Develop leadership skills to advance your career.",
+        points: ["Team management and motivation.", "Leadership strategies for administrative roles.", "Problem-solving and decision-making in a supervisory context."],
+      },
+      {
+        title: "Professional Development and Networking",
+        description: "Build a personal brand and expand your professional network.",
+        points: ["Building a personal brand in customer service and administration.", "Networking strategies for career growth.", "Staying updated on industry trends."],
+      },
+    ],
+    enrolmentMessage: "Enroll now to enhance your Customer Service & Administration skills and stand out in today's competitive market.",
+    enrolmentMessages: [
+      "Enrol Today and Transform Your Career in Customer Service & Administration!",
+      "Quack Recruitment & Training is dedicated to helping you achieve your career aspirations in customer service and administration. Enrol in our Level 3 Course today and embark on a journey toward professional excellence.",
+      "Don't miss this opportunity to enhance your skills, gain confidence, and open doors to exciting career prospects. Contact us now to learn more about our course details, schedules, and enrolment process.",
+      "Invest in your future with Quack Recruitment & Training - where your success begins!",
+    ],
+    price: "£399 + VAT",
+    priceInfo: "Either Government Funded T&Cs apply, or £399 + VAT pp, cheaper quotes available for multiple purchases.",
+    timeToComplete: "c37/40 hours",
+    locations: ["Walsall", "Birmingham", "Northampton", "Luton"],
+    link: "/training/customer-service-administration ",
+  },
+  {
+    title: "CSCS (Construction Skills Certification Scheme)",
+    description: "Prepare for success in the construction industry through our CSCS training:",
+    openingMessages: [
+      "Welcome to Quack Recruitment & Training!",
+      "Unlocking Your Potential with CSCS Level 3 Training.",
+      "At Quack Recruitment & Training, we believe in empowering all individuals to reach new heights in their careers.",
+      "Our comprehensive Level 3 Course in Construction Skills Certification Scheme (CSCS) is designed to equip you with the knowledge and skills necessary to excel in the dynamic and rewarding field of construction.",
+    ],
+    whyChoose: [
+      {
+        title: "Expert Instructors",
+        description:
+          "Our team of experienced instructors are industry professionals with a wealth of knowledge in construction and CSCS requirements. They are committed to providing top-notch training, ensuring you receive the highest quality education to succeed in the construction industry.",
+      },
+      {
+        title: "Tailored Curriculum",
+        description:
+          "Our Level 3 Course is carefully crafted to cover all aspects of the CSCS certification, focusing on both theoretical understanding and practical application. The curriculum is regularly updated to align with industry standards, keeping you ahead in the ever-evolving construction landscape.",
+      },
+      {
+        title: "Flexible Learning Options",
+        description:
+          "We understand the challenges that adult learners face. That's why we offer flexible learning options, including evening classes and weekend sessions. Whether you are working full-time or managing other commitments, we offer a schedule that suits your lifestyle.",
+      },
+      {
+        title: "State-of-the-Art Facilities",
+        description:
+          "We take pride in offering state-of-the-art training facilities. Our classrooms are equipped with the latest technology, providing a conducive environment for effective learning. Additionally, our practical training areas simulate real-world construction scenarios, giving you hands-on experience.",
+      },
+      {
+        title: "Flexible Funding Options",
+        description:
+          "Irrespective of your funding option we are here to help, whether the course is being funded through the DWP, Employer or Personal Savings we have a multitude of payment options available to make sure we can help you achieve the qualification to help you make that next step in your career.",
+      },
+    ],
+    whatYouLearn: [
+      {
+        title: "Health and Safety in Construction",
+        description: "Prioritize safety with in-depth knowledge of construction site hazards.",
+        points: ["Understanding and implementing safety regulations.", "Identifying potential hazards on construction sites.", "Creating a safe working environment."],
+      },
+      {
+        title: "Construction Technology and Techniques",
+        description: "Acquire hands-on skills crucial for the construction sector.",
+        points: ["Fundamentals of construction materials and methods.", "Reading and interpreting construction drawings.", "Practical application of construction techniques."],
+      },
+      {
+        title: "Supervisory Skills",
+        description: "Develop leadership skills to advance your career.",
+        points: ["Effective communication and leadership.", "Conflict resolution and team management.", "Project planning and organization."],
+      },
+      {
+        title: "CSCS Exam Preparation",
+        description: "Prepare for the CSCS exam with our comprehensive training.",
+        points: ["In-depth review of exam topics.", "Practice exams and quizzes.", "Tips and strategies for exam success."],
+      },
+    ],
+    enrolmentMessage: "Embark on your CSCS training journey with us and build a solid foundation for a thriving career in construction.",
+    enrolmentMessages: [
+      "Enrol Today and Secure Your Future in Construction!",
+      "Quack Recruitment & Training is committed to helping you achieve your goals in the construction industry. By enrolling in our Level 3 Course in CSCS, you are taking the first step towards a rewarding and fulfilling career. Don't miss out on this opportunity to enhance your skills, boost your confidence, and open doors to new career possibilities. Contact us today to learn more about our course offerings, schedule, and enrolment process. Invest in your future with Quack Recruitment & Training - where success begins!",
+    ],
+    price: "£399 + VAT",
+    priceInfo: "Either Government Funded T&Cs apply, or £399 + VAT pp, cheaper quotes available for multiple purchases.",
+    timeToComplete: "c37/40 hours",
+    locations: ["Walsall", "Birmingham", "Northampton", "Luton"],
+    link: "/training/cscs",
+  },
+
+  {
+    title: "SIA (Security Industry Authority) Training",
+    description: "Become a proficient security professional with our SIA training:",
+    whatYouLearn: [
+      { title: "Security Protocols", description: "Learn the latest security measures and protocols." },
+      { title: "Conflict Resolution", description: "Acquire skills to handle challenging situations peacefully." },
+      { title: "Legal Compliance", description: "Understand the legal aspects of security operations." },
+      { title: "Practical Training", description: "Gain hands-on experience in real-world security scenarios." },
+    ],
+    enrolmentMessage: "Enroll in our SIA training to step confidently into the security industry with the knowledge and skills you need.",
+    price: "£399 + VAT",
+    priceInfo: "Either Government Funded T&Cs apply, or £399 + VAT pp, cheaper quotes available for multiple purchases.",
+    timeToComplete: "c37/40 hours",
+    locations: ["Walsall", "Birmingham", "Northampton", "Luton"],
+    link: "/training/sia",
+  },
+  {
+    title: "Food and Hygiene Courses",
+    description: "Ensure food safety and hygiene with our specialized courses:",
+    openingMessages: [
+      "Welcome to Quack Recruitment & Training!",
+      "Elevate Your Career with Our Level 3 Food and Hygiene Course for Adults",
+      "Quack Recruitment & Training is your pathway to excellence in the food industry. Our Level 3 Food and Hygiene Course is meticulously crafted to equip you with the knowledge and skills required for a successful and fulfilling career in food safety and hygiene.",
+    ],
+    whyChoose: [
+      {
+        title: "Experienced Instructors",
+        description:
+          "Our team of instructors consists of seasoned professionals with extensive experience in the food and hygiene industry. They are passionate about imparting their knowledge and ensuring that you receive the highest quality training.",
+      },
+      {
+        title: "Comprehensive Curriculum",
+        description:
+          "Our Level 3 Course is designed to cover all aspects of food safety and hygiene. From understanding regulatory requirements to practical application in real-world scenarios, our curriculum is tailored to provide you with a well-rounded education.",
+      },
+      {
+        title: "Practical Training Environments",
+        description:
+          "At Quack Recruitment & Training, we believe in learning by doing. Our training facilities are equipped with state-of-the-art kitchens and simulated food production environments. This hands-on approach allows you to apply theoretical knowledge in practical situations, ensuring that you are job-ready upon completion.",
+      },
+      {
+        title: "Flexible Learning Options",
+        description:
+          "We understand that adult learners have unique scheduling challenges. That's why we offer flexible learning options, including evening classes and weekend sessions. Our goal is to make quality education accessible to everyone, regardless of their current commitments.",
+      },
+    ],
+    whatYouLearn: [
+      {
+        title: "Food Safety Regulations and Legislation",
+        description: "Understand and comply with local and national regulations.",
+        points: ["Implementing best practices for food safety.", "Understanding and complying with local and national regulations."],
+      },
+      {
+        title: "Hazard Analysis and Critical Control Points (HACCP)",
+        description: "Identify and control potential hazards in food production.",
+        points: ["Developing and implementing HACCP plans.", "Identifying and controlling potential hazards in food production."],
+      },
+      {
+        title: "Food Handling and Storage",
+        description: "Learn safe and hygienic food handling and storage techniques.",
+        points: ["Safe and hygienic handling of food.", "Proper storage and preservation techniques."],
+      },
+      {
+        title: "Quality Assurance and Auditing",
+        description: "Conduct internal and external audits.",
+        points: ["Implementing quality assurance processes.", "Conducting internal and external audits."],
+      },
+    ],
+    enrolmentMessage: "Join our food and hygiene courses to guarantee the highest standards of safety and cleanliness in the food industry.",
+    enrolmentMessages: [
+      "Enrol Today and Transform Your Career in Food and Hygiene!",
+      "Quack Recruitment & Training is committed to helping you achieve excellence in the food industry. Enrol in our Level 3 Food and Hygiene Course today and embark on a journey towards a rewarding career in food safety and hygiene.",
+      "Don't miss this opportunity to enhance your skills, gain industry-recognised certifications, and open doors to exciting career prospects. Contact us now to learn more about our course details, schedules, and enrolment process.",
+      "Invest in your future with Quack Recruitment & Training - where your journey to success begins!",
+    ],
+    price: "£399 + VAT",
+    priceInfo: "Either Government Funded T&Cs apply, or £399 + VAT pp, cheaper quotes available for multiple purchases.",
+    timeToComplete: "c37/40 hours",
+    locations: ["Walsall", "Birmingham", "Northampton", "Luton"],
+    link: "/training/food-and-hygiene",
+  },
+];
