6529cc0c2ca74b06df67ac67a252753ed98940db
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-12-14 20:25:52 +0100

    added branch addresses to contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,8 @@
 import BannerHalf from "@/components/Banner/BannerHalf";
 import Section from "@/components/Sections/Section";
 import GeneralContactForm from "@/components/ContactForms/GeneralContactForm/GeneralContactForm";
+import { quackRecruitmentLocations } from "@/components/data/locations";
+import ExpandableSectionFAQ from "@/components/ExpandableSection/ExpandableSectionFAQ";
 
 export const metadata = {
   title: "Contact | Quack Recruitment & Training",
@@ -64,30 +66,42 @@ const Contact = () => {
             </div>
           </div>
         </Section>
-        {/* <Section bgColor="bg-gray bg-opacity-10 text-center">
-          <h2 className="text-2xl font-bold leading-8 text-black sm:text-3xl sm:leading-9 text-center mb-10">Our Locations</h2>
-          <div></div>
-        </Section> */}
         <Section bgColor="bg-gray-50 text-center">
-          <h2 className="text-2xl font-bold leading-8 text-black sm:text-3xl sm:leading-9 text-center mb-10">Serviced Locations & Offices</h2>
-          <div
-            className="relative max-w-screen-lg mx-auto"
-            style={{
-              height: "70vh",
-            }}
-          >
-            <iframe
-              src="https://www.google.com/maps/d/u/0/embed?mid=1Jt4IhkKUr8cE6T5kf97Rs3f8TqFwoMA&ehbc=2E312F&noprof=1"
-              title="Offices & Serviced Locations"
+          <h2 className="text-2xl font-bold leading-8 text-black sm:text-3xl sm:leading-9 text-center mb-10">Our Branches</h2>
+          <div className="lg:flex">
+            <div className="lg:w-1/3 mb-10">
+              <h3 className="text-2xl font-semibold mb-3">Find A Branch Near You</h3>
+              {quackRecruitmentLocations.map((location, index) => (
+                <ExpandableSectionFAQ title={location.area} key={index}>
+                  <address className="mt-2 text-lg leading-relaxed text-left">
+                    <p>Quack Recruitment</p>
+                    {location.address.map((line, addrIndex) => (
+                      <p key={addrIndex}>{line}</p>
+                    ))}
+                    <p>{location.postalCode}</p>
+                  </address>
+                </ExpandableSectionFAQ>
+              ))}
+            </div>
+            <div
+              className="relative lg:w-3/5 max-w-screen-lg mx-auto"
               style={{
-                position: "absolute",
-                top: "0",
-                left: "0",
-                height: "100%",
-                width: "100%",
-                border: "none",
+                height: "60vh",
               }}
-            ></iframe>
+            >
+              <iframe
+                src="https://www.google.com/maps/d/embed?mid=1AEQ0AIORpHpTdUN1ZYW8NfvsZR_yjvM&ehbc=2E312F&noprof=1"
+                title="Offices & Serviced Locations"
+                style={{
+                  position: "absolute",
+                  top: "0",
+                  left: "0",
+                  height: "100%",
+                  width: "100%",
+                  border: "none",
+                }}
+              ></iframe>
+            </div>
           </div>
         </Section>
       </main>
---
+++ src/components/data/locations.js
@@ -0,0 +1,8 @@
+export const quackRecruitmentLocations = [
+  { area: "Hertfordshire (Head Office)", name: "Quack Recruitment", address: ["Business & Technology Centre", "Bessemer Drive", "Stevenage"], postalCode: "SG1 2DX" },
+  { area: "WestMidlands", name: "Quack Recruitment", address: ["20 Colmore Circus Queensway", "Birmingham"], postalCode: "B4 6AT" },
+  { area: "EastMidlands", name: "Quack Recruitment", address: ["Manor House Drive", "Coventry"], postalCode: "CV1 2TE" },
+  { area: "Bedfordshire", name: "Quack Recruitment", address: ["960 Capability Green", "Luton"], postalCode: "LU1 3PE" },
+  { area: "Buckinghamshire", name: "Quack Recruitment", address: ["Midsummer Court", "314 Midsummer Blvd", "Milton Keynes"], postalCode: "MK9 2UB" },
+  { area: "Northamptonshire", name: "Quack Recruitment", address: ["Victory House", "Business Park", "400 Pavilion Dr", "Northampton"], postalCode: "NN4 7PA" },
+];
