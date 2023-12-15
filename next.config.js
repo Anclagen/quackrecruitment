b52e89f95c040c42c47d55dab408154c47aa1168
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-12-15 10:12:18 +0100

    403 on ionos build fix?

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   },
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+  trailingSlash: true,
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
